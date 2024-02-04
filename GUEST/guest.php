117fcfbaf804bce5b92030ce7210101707063916
nais <123295006+NICE-HUB@users.noreply.github.com>
2024-02-04 22:17:40 +0300

    added files and modified others

--- GUEST/guest.php
+++ GUEST/guest.php
@@ -6,19 +6,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
     <link href="STYLES/style.css" rel="stylesheet">
     <title>Document</title>
-    <header>
-      <a href=""><img class="logo" src="hacker.png" alt="" width="50px" height="50px"></a>
-      <nav>
-          <ul class="nav_links">
-              <li><a href="#">HOME</a></li>
-              <li><a href="#">COURSES</a></li>
-              <li><a href="#">ABOUT</a></li>
-              <li><a href="#">CONTACT</a></li>
-          </ul>
-      </nav>
-      <i class="fa-solid fa-magnifying-glass"></i>
-      <a class="cta" href="/FORMS/login.php"><button id="user">login<i class="fa-solid fa-user"></i></button></a>
-  </header>
+    <header> </header>
     <style>
 
       @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=PT+Sans&family=Roboto:wght@300&display=swap');
@@ -105,7 +93,7 @@ button:hover{
         display: flex;
         padding: 10px;
       }
-      .container .txt {       
+      .txt {       
         font-size: 48px;
         line-height: 1;
         text-align: left;
@@ -237,6 +225,41 @@ button:hover{
   text-align: center;
   background-color: #f1f1f1;
 }
+.reviews-message {
+            display: flex;
+            justify-content: space-around; /* Adjust as needed */
+            background-color: black;
+            padding-top: 30px ;
+            color: white;
+            border-width: 10px;
+        }
+
+        .reviews {
+            transition: 0.3s;
+            width: 20%;
+            display: flex;
+            flex-direction: column; /* Stack items vertically */
+            align-items: center;
+        }
+
+        .reviews:hover {
+            box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+        }
+
+        .message {
+            padding: 2px 16px;
+            text-align: center;
+        }
+        .colabs {
+          height: 70px;
+        }
+
+        .colabs img {
+            width: 40px;
+            height: 40px;
+            margin-right: 100px; /* Adjust the margin to control spacing between images */
+        }
+
     </style>
 </head>
 <body>
@@ -248,7 +271,7 @@ button:hover{
                the last frame in an animation, Skillshare  <br>
                 is here to support you on every <br>
                  step of your creative journey.</p>
-            <button onclick="window.location.href='HOME/index.html';" >Learn More</button>
+            <button onclick="window.location.href='../FORMS/signup.php'" >Learn More</button>
             
         </div>
         <div class="courses-container" id="cosescontainer">
@@ -256,7 +279,7 @@ button:hover{
             <img class="courses-image" src="https://www.bloemfonteincourant.co.za/wp-content/uploads/2018/07/digital-coding-785.jpg" alt="Course 2">
             <h3>BSE COURSE</h3>
             <p>
-              <button class="unit-btn" onclick="window.location.href='#BSE';">
+              <button class="unit-btn" onclick="window.location.href='../COURSES/courses.php';">
                 321 units
               </button>
             </p>
@@ -265,7 +288,7 @@ button:hover{
             <img class="courses-image" src="https://www.bloemfonteincourant.co.za/wp-content/uploads/2018/07/digital-coding-785.jpg" alt="Course 2">
             <h3>BCS COURSE</h3>
             <p>
-              <button class="unit-btn" onclick="window.location.href='#BCS';">
+              <button class="unit-btn" onclick="window.location.href='../COURSES/courses.php';">
                 321 units
               </button>
             </p>
@@ -274,7 +297,7 @@ button:hover{
             <img class="courses-image" src="https://www.bloemfonteincourant.co.za/wp-content/uploads/2018/07/digital-coding-785.jpg" alt="Course 2">
             <h3>BBIT COURSE</h3>
             <p>
-              <button class="unit-btn" onclick="window.location.href='#BBIT';">
+              <button class="unit-btn" onclick="window.location.href='../COURSES/courses.php';">
                 321 units
               </button>
             </p>
@@ -283,7 +306,7 @@ button:hover{
             <img class="courses-image" src="https://www.bloemfonteincourant.co.za/wp-content/uploads/2018/07/digital-coding-785.jpg" alt="Course 2">
             <h3>BSCIT COURSE</h3>
             <p>
-              <button class="unit-btn" onclick="window.location.href='#BIT';">
+              <button class="unit-btn" onclick="window.location.href='../COURSES/courses.php';">
                 321 units
               </button>
             </p>
@@ -292,7 +315,7 @@ button:hover{
             <img class="courses-image" src="https://www.bloemfonteincourant.co.za/wp-content/uploads/2018/07/digital-coding-785.jpg" alt="Course 2">
             <h3>IT COURSE</h3>
             <p>
-              <button class="unit-btn" onclick="window.location.href='#BCOM';">
+              <button class="unit-btn" onclick="window.location.href='../COURSES/courses.php';">
                 321 units
               </button>
             </p>
@@ -301,7 +324,7 @@ button:hover{
             <img class="courses-image" src="https://www.bloemfonteincourant.co.za/wp-content/uploads/2018/07/digital-coding-785.jpg" alt="Course 2">
             <h3>BCT COURSE</h3>
             <p>
-              <button class="unit-btn" onclick="window.location.href='#SE';">
+              <button class="unit-btn" onclick="window.location.href='../COURSES/courses.php';">
                 321 units
               </button>
             </p>
@@ -310,7 +333,7 @@ button:hover{
             <img class="courses-image" src="https://www.bloemfonteincourant.co.za/wp-content/uploads/2018/07/digital-coding-785.jpg" alt="Course 2">
             <h3>BSE COURSE</h3>
             <p>
-              <button class="unit-btn" onclick="window.location.href='#BSE';">
+              <button class="unit-btn" onclick="window.location.href='../COURSES/courses.php';">
                 321 units
               </button>
             </p>
@@ -319,7 +342,7 @@ button:hover{
             <img class="courses-image" src="https://www.bloemfonteincourant.co.za/wp-content/uploads/2018/07/digital-coding-785.jpg" alt="Course 2">
             <h3>BCS COURSE</h3>
             <p>
-              <button class="unit-btn" onclick="window.location.href='#BCS';">
+              <button class="unit-btn" onclick="window.location.href='../COURSES/courses.php';">
                 321 units
               </button>
             </p>
@@ -328,7 +351,7 @@ button:hover{
             <img class="courses-image" src="https://www.bloemfonteincourant.co.za/wp-content/uploads/2018/07/digital-coding-785.jpg" alt="Course 2">
             <h3>BSCIT COURSE</h3>
             <p>
-              <button class="unit-btn" onclick="window.location.href='#BIT';">
+              <button class="unit-btn" onclick="window.location.href='../COURSES/courses.php';">
                 321 units
               </button>
             </p>
@@ -337,7 +360,7 @@ button:hover{
             <img class="courses-image" src="https://www.bloemfonteincourant.co.za/wp-content/uploads/2018/07/digital-coding-785.jpg" alt="Course 2">
             <h3>BCT COURSE</h3>
             <p>
-              <button class="unit-btn" onclick="window.location.href='#SE';">
+              <button class="unit-btn" onclick="window.location.href='../COURSES/courses.php';">
                 321 units
               </button>
             </p>
@@ -346,7 +369,7 @@ button:hover{
             <img class="courses-image" src="https://www.bloemfonteincourant.co.za/wp-content/uploads/2018/07/digital-coding-785.jpg" alt="Course 2">
             <h3>BIT COURSE</h3>
             <p>
-              <button class="unit-btn" onclick="window.location.href='#SE';">
+              <button class="unit-btn" onclick="window.location.href='../COURSES/courses.php';">
                 321 units
               </button>
             </p>
@@ -355,13 +378,14 @@ button:hover{
             <img class="courses-image" src="https://www.bloemfonteincourant.co.za/wp-content/uploads/2018/07/digital-coding-785.jpg" alt="Course 2">
             <h3>BCOT COURSE</h3>
             <p>
-              <button class="unit-btn" onclick="window.location.href='#SE';">
+              <button class="unit-btn" onclick="window.location.href='../COURSES/courses.php';">
                 321 units
               </button>
             </p>
           </div>
         </div>
     </div>
+    <br>
     <div class="more" id="mocoses">
       <a href="#rodmps">Show more courses <i class="fa fa-sort-desc" aria-hidden="true"></i></a>
     </div>
@@ -370,6 +394,7 @@ button:hover{
       <div class="row">
         <div class="column">
           <div class="card">
+          <a href="../COURSES/courses.php">
             <img src="https://cdn.sanity.io/images/tlr8oxjg/production/ada93729daf922ad0318c8c0295e5cb477921808-1456x816.png?w=3840&q=100&fit=clip&auto=format"> 
             <p>DATA SCIENCE</p>
             <p>5.0<span class="fa fa-star checked"></span>
@@ -378,10 +403,12 @@ button:hover{
               <span class="fa fa-star checked"></span>
               <span class="fa fa-star checked"></span></p>
               <p>40$</p>
+              </a>
           </div>
         </div>
         <div class="column">
           <div class="card">
+          <a href="../COURSES/courses.php">
             <img src="https://cdn.sanity.io/images/tlr8oxjg/production/ada93729daf922ad0318c8c0295e5cb477921808-1456x816.png?w=3840&q=100&fit=clip&auto=format">
             <p>WEB DEVELOPMENT</p>
             <p>2.0<span class="fa fa-star checked"></span>
@@ -390,10 +417,12 @@ button:hover{
               <span class="fa fa-star"></span>
               <span class="fa fa-star"></span></p>
               <p>80$</p>
+              </a>
           </div>
         </div>
         <div class="column">
           <div class="card">
+          <a href="../COURSES/courses.php">
             <img src="https://cdn.sanity.io/images/tlr8oxjg/production/ada93729daf922ad0318c8c0295e5cb477921808-1456x816.png?w=3840&q=100&fit=clip&auto=format">
             <p>DATA ANALYTICS</p>
             <p>3.0<span class="fa fa-star checked"></span>
@@ -402,10 +431,12 @@ button:hover{
               <span class="fa fa-star"></span>
               <span class="fa fa-star"></span></p>
               <p>40$</p>
+              </a>
           </div>
         </div>
         <div class="column">
           <div class="card">
+          <a href="../COURSES/courses.php">
             <img src="https://cdn.sanity.io/images/tlr8oxjg/production/ada93729daf922ad0318c8c0295e5cb477921808-1456x816.png?w=3840&q=100&fit=clip&auto=format">
             <p>DATA STRUCTURES</p>
             <p>4.5<span class="fa fa-star checked"></span>
@@ -413,11 +444,12 @@ button:hover{
               <span class="fa fa-star checked"></span>
               <span class="fa fa-star checked"></span>
               <span class="fa fa-star"></span></p>
-              v
+              </a>
           </div>
         </div>
         <div class="column">
           <div class="card">
+          <a href="../COURSES/courses.php">
             <img src="https://cdn.sanity.io/images/tlr8oxjg/production/ada93729daf922ad0318c8c0295e5cb477921808-1456x816.png?w=3840&q=100&fit=clip&auto=format">
             <p>MACHINE LEARNING</p>
             <p>3.0<span class="fa fa-star checked"></span>
@@ -426,10 +458,12 @@ button:hover{
               <span class="fa fa-star"></span>
               <span class="fa fa-star"></span></p>
               <p>100$</p>
+              </a>
           </div>
         </div>
         <div class="column">
           <div class="card">
+          <a href="../COURSES/courses.php">
             <img src="https://cdn.sanity.io/images/tlr8oxjg/production/ada93729daf922ad0318c8c0295e5cb477921808-1456x816.png?w=3840&q=100&fit=clip&auto=format">
             <p>APP  DEVELOPMEN <i>java</i> </p>
             <p><span class="fa fa-star "></span>
@@ -438,10 +472,12 @@ button:hover{
               <span class="fa fa-star"></span>
               <span class="fa fa-star"></span></p>
               <p>400$</p>
+              </a>
           </div>
         </div>
         <div class="column">
           <div class="card">
+            <a href="../COURSES/courses.php">
             <img src="https://cdn.sanity.io/images/tlr8oxjg/production/ada93729daf922ad0318c8c0295e5cb477921808-1456x816.png?w=3840&q=100&fit=clip&auto=format">
             <p>FLUTTER <i>dart</i> </p>
             <p>5.0<span class="fa fa-star checked"></span>
@@ -450,19 +486,86 @@ button:hover{
               <span class="fa fa-star checked"></span>
               <span class="fa fa-star checked"></span></p>
               <p>150$</p>
+              </a>
           </div>
         </div>
-        </div>
+        <div class="column">
+          <div class="card">
+          <a href="../COURSES/courses.php"></a>
+            <img src="https://cdn.sanity.io/images/tlr8oxjg/production/ada93729daf922ad0318c8c0295e5cb477921808-1456x816.png?w=3840&q=100&fit=clip&auto=format">
+            <p>Some text</p>
+            <p>3.0<span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span></p>
+          </div>
+        </div> 
       </div>
     </div>
     <div class="backtop" id="baktop">
       <a href="#cosescontainer"><h3>Back to courses</h3></a>
     </div>
 <br>
+    <br>
+    
     <h2 style="text-align:center">What our students are saying </h2>
+    <br>
+    <br>
+    <hr>
+    <br>
+    <div class="reviews-message">
 
-   
+        <div class="reviews">
+            <img src="boy.png" alt="Avatar" style="width:100%">
+            <div class="message">
+                <h4><b>John Doe</b></h4>
+                <p>It has been a really great experience, very educative and has allowed me to step out of my comfort zone. I have enjoyed every minute of the lesson and was always looking forward to the next.</p>
+            </div>
+        </div>
 
-</div>
+        <div class="reviews">
+            <img src="girl.jpg" alt="Avatar" style="width:100%">
+            <div class="message">
+                <h4><b>John Doe</b></h4>
+                <p>What an amazing class! Thanks Matt and team for your well-thought out classes and prompt response every time my code didn't work. I've had a lot of fun with the Pro class but also learnt a lot. <br>  It was especially exciting working on the weather app. Thank you and see you on the next one!</p>
+            </div>
+        </div>
+
+        <div class="reviews">
+            <img src="girly.jpg" alt="Avatar" style="width:100%">
+            <div class="message">
+                <h4><b>John Doe</b></h4>
+                <p>I'm so grateful that as we wind up the year I am finishing with a purpose and a skill that I know will change my life for the better. <br> I'm so grateful for this platform and the opportunity. <br>  Happy to be a part of the shecodes community.</p>
+            </div>
+        </div>
+
+    </div>
+    <br>
+    <div class="colabs">
+      <marquee>
+        <img src="girly.jpg" style="width:40px; height:40px">
+        <img src="ChatGPT.png" style="width:45px; height:40px">
+        <img src="Google.jpg" style="width:40px; height:40px">
+        <img src="codecademy.avif" style="width:40px; height:40px">
+        <img src="alx.jpg" style="width:45px; height:40px">
+        <img src="girly.jpg" style="width:40px; height:40px">
+        <img src="ChatGPT.png" style="width:45px; height:40px">
+        <img src="Google.jpg" style="width:40px; height:40px">
+        <img src="codecademy.avif" style="width:40px; height:40px">
+        <img src="alx.jpg" style="width:45px; height:40px">
+        <img src="girly.jpg" style="width:40px; height:40px">
+        <img src="ChatGPT.png" style="width:45px; height:40px">
+        <img src="Google.jpg" style="width:40px; height:40px">
+        <img src="codecademy.avif" style="width:40px; height:40px">
+        <img src="alx.jpg" style="width:45px; height:40px">
+      </marquee>
+    </div>
+
+   <footer>
+
+
+   </footer>
+
 </body>
 </html>
